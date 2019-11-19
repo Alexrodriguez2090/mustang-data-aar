2773d65011c27fcd20b72f8038b5561bb4f58b14
Alexrodriguez2090 <54908770+Alexrodriguez2090@users.noreply.github.com>
2019-11-18 18:06:31 -0600

    Add files via upload

--- contactparsing.js
+++ contactparsing.js
@@ -158,16 +158,3 @@ function delContact() {
 	override();
     contactMarker();
 }
-
-var demo1 = new autoComplete({
-selector: 'document.getElementById("nameid").value',
-minChars: 1,
-source: function(term, suggest){
-    term = term.toLowerCase();
-    var choices = ['ActionScript', 'AppleScript', 'Asp', 'Assembly', 'BASIC', 'Batch', 'C', 'C++', 'CSS', 'Clojure', 'COBOL', 'ColdFusion', 'Erlang', 'Fortran', 'Groovy', 'Haskell', 'HTML', 'Java', 'JavaScript', 'Lisp', 'Perl', 'PHP', 'PowerShell', 'Python', 'Ruby', 'Scala', 'Scheme', 'SQL', 'TeX', 'XML'];
-    var matches = [];
-    for (i=0; i<choices.length; i++)
-        if (~choices[i].toLowerCase().indexOf(term)) matches.push(choices[i]);
-    suggest(matches);
-}
-});
